aee96b9fe95b4c38505a5ea3b0abc552c1e60b2b
phucan2411 <nguyenphucan24112003@gmail.com>
2023-05-04 11:11:46 +0700

    ll

---
+++ app/views/cart/index.php
@@ -0,0 +1,34 @@
+<div class="container" style="padding-top:10rem;">
+    <h1 class="text-center">Giỏ Hàng Của Bạn</h1>
+    <table class="table mt-4">
+        <thead>
+            <tr>
+                <th>Tên Sản phẩm</th>
+                <th>Hình </th>
+                <th>Mô Tả</th>
+                <th>Giá</th>
+                <th>Số Lượng</th>
+                <th>Tổng cộng</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Product 1</td>
+                <td><img src="" alt="error"></td>
+                <td>Mô Tả</td>
+                <td>$10.00</td>
+                <td><input type="number" value="1" min="1"></td>
+                <td>$10.00</td>
+                <td ><a href="" style="color:red ;">X</a></td>
+            </tr>
+        </tbody>
+    </table>
+    <div class="text-end mt-4">
+        <h5>Thành tiền: $60.00</h5>
+    </div>
+    <div class="text-center mt-4">
+        <button class="btn btn-danger">Tiếp Tục Mua Hàng</button>
+        <button class="btn btn-success">Thanh Toán</button>
+    </div>
+</div>
