de53a302140992acfcb78a93a29c398c7d4ae42e
phucan2411 <nguyenphucan24112003@gmail.com>
2023-05-08 11:44:14 +0700

    test

--- app/controllers/Shopcat.php
+++ app/controllers/Shopcat.php
@@ -2,20 +2,17 @@
 class ShopCat extends Controller
 {
     public $data = [], $link = "shopcat/index";
-
     public function index()
     {
         $id_pet=2;
         $products = $this->model("ProductModel");
         // // require_once.....
         // $this->data['sub_content']['productsCatNoSale'] = $products ->getProductCatAllNoSale();
-
         $this->data['sub_content']['categories'] = $products->getCategories();
         $this->data['sub_content']['productsSaleCat'] = $products->getProductSaleCatAll();
         $this->data['sub_content']['productsCat'] = $products->getProductAll($id_pet);
         $this->data['sub_content']['CountCat'] = $products->getCountProduct($id_pet);
-
-
+        
         $this->data['content'] =  $this->link; // đường dẫn tới file view
 
         // Render Views
